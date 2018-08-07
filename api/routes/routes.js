5a26d4dbe28aaaccc629792e3d4f1f8feabaa2cb
Glitch (mirror-starfish) <none>
2018-08-07 01:12:43 +0000

    🚓🚎 Checkpoint ./api/routes/routes.js:531805/222

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,18 +2,18 @@ module.exports = {
   
   routes: function(app){
     app.route('/api/exercise/new-user')
-    .get((req,res) => {
+    .post((req,res) => {
       res.send("add new user");
     })
     
     app.route('/api/exercise/add')
-    .get((req,res) => {
+    .post((req,res) => {
       res.send("log new exercise");
     })
     
-    app.route('/api/exercise/log?{userId}[&amp;from][&amp;to][&amp;limit]')
+    app.route('/api/exercise/log?:userId&:from]&:to&:limit')
     .get((req,res) => {
-      res.send("find exercise logs");
+      res.json({"result": req.query});
     })
   }
   
