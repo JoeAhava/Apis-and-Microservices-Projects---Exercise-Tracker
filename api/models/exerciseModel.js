2a7528eaace7f61f76753002eac3fdade6410f46
Glitch (mirror-starfish) <none>
2018-08-08 03:59:44 +0000

    📷🖇 Checkpoint ./api/models/exerciseModel.js:531805/2

--- api/models/exerciseModel.js
+++ api/models/exerciseModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const exerciseSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
-    ref: 'user',
+    ref: 'User',
     required: true
   },
   description: {
