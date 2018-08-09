95f6d4b0eb8c92450c0b2a960327204d1ab6a7f8
Glitch (mirror-starfish) <none>
2018-08-09 09:16:31 +0000

    🚃👯 Checkpoint ./api/controllers/handleExercise.js:531805/207

--- api/controllers/handleExercise.js
+++ api/controllers/handleExercise.js
@@ -8,7 +8,7 @@ const addExercise = (req,res) => {
   const userId = req.body.userId;
   const description = req.body.description;
   const duration = parseInt(req.body.duration);
-  const date = Date.parse(Date(req.body.date));
+  const date = (new Date(req.body.date)).getTime();
   
   
   
@@ -40,13 +40,11 @@ const addExercise = (req,res) => {
 const searchExercise = (req,res) => {
   
   const user = req.query.userId;
-  let from = Date.parse(Date(req.query.from));
-  let to = Date.parse(Date(req.query.to));
+  let from = (new Date(req.query.from).getTime())-1;
+  let to = (new Date(req.query.to).getTime())+1;
   let limit = req.query.limit;
   
-  console.log(req.query);
-  console.log(from);
-  console.log(to);
+  
   
   Exercise.find({user: user, date: { $gt: 10, $lt: 1533735608000}}, (err, exercises) => {
     if(err){
